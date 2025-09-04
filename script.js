b150c3244ac22699d852b5a570b46ebb1c0c40c2
KimberllyMedeiros <kimkahmedeiros@gmail.com>
2025-09-03 21:45:52 -0300

    finalizado a sintaxe

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 
-document.getElementsByClassName(".box")
-const box= document.querySelectorAll(".box")
-console.log(box.length)
+
+
+const botao1= document.getElementById("but1")
+const box1= document.getElementById("box1")
+
+botao1.addEventListener("click", () =>{
+    box1.classList.add("box1")
+})
+const botao2= document.getElementById("but2")
+const box2= document.getElementById("box2")
+botao2.addEventListener("click", () =>{
+    texto2.innerText = "all change rs"
+    box2.classList.add("box2")
+    but2.classList.add("but2")
+    but2.innerText = "wow"
+})
+const botao3= document.getElementById("but3")
+const box3= document.getElementById("box3")
+
+botao3.addEventListener("click", () =>{
+    box3.classList.add("box3")
+})
+const botao4= document.getElementById("but4")
+const box4= document.getElementById("box4")
+
+botao4.addEventListener("click", () =>{
+    box4.classList.add("box4")
+})
