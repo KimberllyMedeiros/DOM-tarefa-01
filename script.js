4bb9dbd222a411a5fd40786db1182c0f4ca7665d
KimberllyMedeiros <kimkahmedeiros@gmail.com>
2025-09-15 21:17:25 -0300

    acabei

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const botao1= document.getElementById("but1")
 const box1= document.getElementById("box1")
 
 botao1.addEventListener("click", () =>{
+     texto1.innerText = "all change rs"
     box1.classList.add("box1")
+    but1.classList.add("but1")
+    but1.innerText = "wow"
 })
 const botao2= document.getElementById("but2")
 const box2= document.getElementById("box2")
@@ -19,11 +22,16 @@ const botao3= document.getElementById("but3")
 const box3= document.getElementById("box3")
 
 botao3.addEventListener("click", () =>{
+    texto3.innerText = "all change rs"
     box3.classList.add("box3")
+    but3.classList.add("but3")
+    but3.innerText = "wow"
 })
 const botao4= document.getElementById("but4")
 const box4= document.getElementById("box4")
-
 botao4.addEventListener("click", () =>{
+    texto4.innerText = "all change rs"
     box4.classList.add("box4")
+    but4.classList.add("but4")
+    but4.innerText = "wow"
 })
